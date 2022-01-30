467653e25a2299a289b6d4b693a8096518cbbeda
caojunjie <1301239018@qq.com>
2022-01-30 11:08:17 +0800

    定义搜索列表

---
+++ index.js
@@ -0,0 +1,8 @@
+// TODO logo
+const searchList = [
+  { name: 'google', url: 'https://www.google.com/search?q={query}' },
+  { name: 'baidu', url: 'https://www.baidu.com/s?wd={query}' },
+  { name: 'baidu', url: 'https://www.baidu.com/s?wd={query}' },
+  { name: 'baidu', url: 'https://www.zhihu.com/search?q={query}' },
+  { name: 'bilibili', url: 'https://search.bilibili.com/all?keyword={query}' }
+];
