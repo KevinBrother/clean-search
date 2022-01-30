042d1241b0a6c4c961e1dd08d84500854d7c7648
caojunjie <1301239018@qq.com>
2022-01-30 14:29:23 +0800

    更新默认搜索列表

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const searchList = [
   { name: 'google', url: 'https://www.google.com/search?q={query}' },
   { name: 'baidu', url: 'https://www.baidu.com/s?wd={query}' },
-  { name: 'baidu', url: 'https://www.baidu.com/s?wd={query}' },
-  { name: 'baidu', url: 'https://www.zhihu.com/search?q={query}' },
+  { name: 'zhihu', url: 'https://www.zhihu.com/search?q={query}' },
+  { name: 'npmjs', url: 'https://www.npmjs.com/package/{query}' },
+  { name: 'mdn', url: 'https://developer.mozilla.org/search?q={query}' },
   { name: 'bilibili', url: 'https://search.bilibili.com/all?keyword={query}' }
 ];
