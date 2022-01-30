17dc9df5b6769faac7d8935ccceef523ed395de8
caojunjie <1301239018@qq.com>
2022-01-30 16:43:59 +0800

    网站添加logo

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 // TODO logo
 const searchList = [
-  { name: 'google', url: 'https://www.google.com/search?q={query}' },
-  { name: 'baidu', url: 'https://www.baidu.com/s?wd={query}' },
-  { name: 'zhihu', url: 'https://www.zhihu.com/search?q={query}' },
-  { name: 'npmjs', url: 'https://www.npmjs.com/package/{query}' },
-  { name: 'mdn', url: 'https://developer.mozilla.org/search?q={query}' },
-  { name: 'bilibili', url: 'https://search.bilibili.com/all?keyword={query}' }
+  {
+    name: 'google',
+    url: 'https://www.google.com/search?q={query}',
+    icon: 'https://www.google.com/favicon.ico'
+  },
+  {
+    name: 'baidu',
+    url: 'https://www.baidu.com/s?wd={query}',
+    icon: 'https://www.baidu.com/favicon.ico'
+  },
+  {
+    name: 'baidu',
+    url: 'https://www.zhihu.com/search?q={query}',
+    icon: 'https://www.zhihu.com/favicon.ico'
+  },
+  {
+    name: 'npmjs',
+    url: 'https://www.npmjs.com/package/{query}',
+    icon: 'https://static.npmjs.com/b0f1a8318363185cc2ea6a40ac23eeb2.png'
+  },
+  {
+    name: 'mdn',
+    url: 'https://developer.mozilla.org/search?q={query}',
+    icon: 'https://developer.mozilla.org/favicon.ico'
+  },
+  {
+    name: 'bilibili',
+    url: 'https://search.bilibili.com/all?keyword={query}',
+    icon: 'https://search.bilibili.com/favicon.ico'
+  }
 ];
